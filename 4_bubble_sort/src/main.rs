f6a8004da6e8b7fa3f3a1c2d1824345ef209a405
m <matus.solcany@protonmail.com>
2023-06-08 11:16:52 +0200

    bubble sort boilerplate

---
+++ 4_bubble_sort/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+              
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+//    #[test]
+//        fn test_search_success() {
+//            let v = vec![1,2,3,4,5];
+//            assert_eq!(bin_search_vec(v, 1), true);
+//    }
+//    #[test]
+//        fn test_search_fail() {
+//            let v = vec![1,2,3,4,5];
+//            assert_eq!(bin_search_vec(v, 1000), false);
+//    }
+
+}
